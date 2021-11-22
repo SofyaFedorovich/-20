81001d9c8516a61df5bd8721995c93f53f921a14
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-22 13:19:58 +0300

    Добавьте файлы проекта.

---
+++ Занятие 20/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Занятие_20
+{//Делегаты, лямбды, события
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
